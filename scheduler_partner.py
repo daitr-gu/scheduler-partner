924ee137c92b8536e04aeef782aaddb90973293f
Dai Tran <dai.tr@geekup.vn>
2014-11-04 14:05:01 +0700

    Update scheduler-partner API

--- scheduler_partner.py
+++ scheduler_partner.py
@@ -17,16 +17,19 @@ import webob.exc
 from nova.api.openstack import extensions
 from nova.api.openstack import wsgi
 from nova.api.openstack import xmlutil
+import nova.db.api as DbAPI
+from pprint import pprint
 
 authorize = extensions.extension_authorizer('compute', 'partner')
 
 
 class PartnerTemplate(xmlutil.TemplateBuilder):
     def construct(self):
-        root = xmlutil.TemplateElement('partner')
-        elem = xmlutil.make_flat_dict('partner', selector='partner',
-                                      subselector='partner')
-        root.append(elem)
+        # root = xmlutil.TemplateElement('scheduler_partner', selector='scheduler')
+        root = xmlutil.TemplateElement('scheduler_partner')
+        # elem = xmlutil.make_flat_dict('scheduler_partner', selector='scheduler_partner',
+        #                               subselector='scheduler_partner')
+        # root.append(elem)
 
         return xmlutil.MasterTemplate(root, 1)
 
@@ -50,14 +53,85 @@ class PartnerController(object):
     @wsgi.serializers(xml=PartnerTemplate)
     def detail(self, req, body=None):
         print "LIST"
-        rval = []
-        rval.append({'partner': 'detail'})
-        return {'partner': rval}
+        return {'partner': {'partner': 'detail'}}
 
     @wsgi.serializers(xml=PartnerTemplate)
-    def create(self, req):
-        print "CREATE"
-        return {'create': 'done'}
+    def create(self, req, body=None):
+        #Define some constant
+        cpus = 8
+        ram = 16384
+        points = {
+            '1': 8,
+            '2': 1,
+            '3': 16,
+            '4': 32,
+            '5': 4
+        }
+        start_max_point = 8
+
+        # pprint(req)
+        # return {'scheduler_partner': {'scheduler': 'ACCEPTED'}}
+
+        ctxt = req.environ['nova.context']
+
+        host = body['host']
+        if host == 'HCMUT':
+            req_from = 'HCMUS'
+        else:
+            req_from = 'HCMUT'
+        req_flavor = DbAPI.flavor_get(ctxt, body['flavor'])
+        req_num_instances = int(body['num_instances'])
+
+        partner = DbAPI.partners_get_by_shortname(ctxt, req_from)
+        pprint(partner)
+        if not partner:
+            return {'scheduler_partner': {'success': 0, 'message': 'You are not our partner! %s' % host}}
+
+        print 'FLAVOR ID'
+        print req_flavor['id']
+        req_point = req_num_instances * points[str(req_flavor['id'])]
+        requested = int(partner['requested'])
+        satisfied = int(partner['satisfied'])
+        limit_ratio = float(partner['limit_ratio'])
+        if requested == 0:
+            if (req_point + satisfied) > start_max_point:
+                return {'scheduler_partner': {'success': 0, 'message': 'Limit ratio exceed! First request is just ' + `start_max_point`}}
+        else:
+            if ((req_point + satisfied) / float(requested)) > limit_ratio:
+                return {'scheduler_partner': {'success': 0, 'message': 'Limit ratio exceed'}}
+
+        instances = DbAPI.temp_instances_get_by_host(ctxt, req_from)
+        used_cpus = 0
+        used_ram = 0
+        for instance in instances:
+            flavor = DbAPI.flavor_get(ctxt, instance.flavor)
+            used_cpus += int(flavor['vcpus'])
+            used_ram += int(flavor['memory_mb'])
+
+        print used_cpus
+        print used_ram
+
+        cpus_need = int(req_flavor['vcpus']) * req_num_instances
+        ram_need = int(req_flavor['memory_mb']) * req_num_instances
+
+        if (used_ram + ram_need) <= ram or (used_cpus + cpus_need) <= cpus:
+
+            DbAPI.partners_update(ctxt, req_from, {
+                'satisfied': req_point + satisfied
+            })
+
+            for i in range(req_num_instances):
+                DbAPI.temp_instances_create({
+                    'host': req_from,
+                    'flavor': req_flavor['id']
+                })
+
+            return {'scheduler_partner': {'success': 1, 'message': 'ACCEPTED', 'points': (req_point + satisfied)}}
+        else:
+            instance_ram_satisfy = (ram - used_ram) // int(req_flavor['memory_mb'])
+            instance_cpu_satisfy = (cpus - used_cpus) // int(req_flavor['vcpus'])
+            satisfy_instance = min(instance_cpu_satisfy, instance_ram_satisfy)
+            return {'scheduler_partner': {'success': 0, 'message': 'REJECTED. We can only satisfy %s instances at this time' % satisfy_instance}}
 
 
 class Scheduler_partner(extensions.ExtensionDescriptor):
@@ -71,8 +145,7 @@ class Scheduler_partner(extensions.ExtensionDescriptor):
     def get_resources(self):
         resources = []
 
-        res = extensions.ResourceExtension('os-scheduler-partner', PartnerController(),
-                                           collection_actions={"create": "POST"})
+        res = extensions.ResourceExtension('os-scheduler-partner', PartnerController())
 
         resources.append(res)
         return resources
